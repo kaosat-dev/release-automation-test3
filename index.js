d6599cf2c3cb92a0edd2f18f84cacc03141fed3e
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-10-30 15:06:18 +0100

    feat(foobar): added foobar feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bar = 'yipee'
 const nope = () => bar
 const fooyeah = () => console.log('fooyeah')
+const foobar = () => console.log('foobar')
 
-module.exports = {nope, fooyeah}
+module.exports = {nope, fooyeah, foobar}
